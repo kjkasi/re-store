8af48567937bd85732e92900a4525ffc9ae03c75
root <root@localhost.localdomain>
2020-05-06 21:07:38 +0300

    7. Роутинг

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
-import { withBookstoreService } from '../hoc';
+import { HomePage, CartPage } from '../pages';
 
 import './app.css';
 
-const App = ({ bookstoreService }) => {
-  console.log(bookstoreService.getBooks());
-  return <div>App</div>;
+const App = () => {
+  return (
+    <Switch>
+      <Route
+        path="/"
+        component={HomePage}
+        exact />
+      <Route
+        path="/cart"
+        component={CartPage}
+        exact />
+    </Switch>
+  );
 };
 
-export default withBookstoreService()(App);
+export default (App);
