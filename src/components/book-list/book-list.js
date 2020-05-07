3e237b6245fb1434119e7f27ecf1e3fbe70bffe9
root <root@localhost.localdomain>
2020-05-07 10:06:50 +0300

    8. Чтение данных из Redux Store

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import BookListItem from '../book-list-item';
 
 import './book-list.css';
@@ -21,4 +22,8 @@ class BookList extends Component {
   };
 };
 
-export default BookList;
+const mapStateToProps = ({ books }) => {
+  return { books };
+};
+
+export default connect(mapStateToProps)(BookList);
